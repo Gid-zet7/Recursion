f921a0b21817882f4f49f5b79b8a3fa6763eb21d
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-10-18 21:35:26 +0000

    Updated palindrome

--- Palindrome/palindrome.js
+++ Palindrome/palindrome.js
@@ -11,20 +11,40 @@ function toChars(str) {
   return ans;
 }
 
-function isPlaindrome(str) {
+// Using the for loops to solve palindome
+function isPalindrome(str) {
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      console.log("Not palindrome");
+      return;
+    }
+  }
+
+  console.log("Palindrome");
+  return;
+}
+
+// isPal("Civic");
+// isPal("Madam");
+// isPal("Dennis sinned");
+// isPal("Able was I ere I saw Elba");
+
+// Using recursion
+function isPalindrome(str) {
   if (str.length <= 1) {
     return true;
   } else {
-    return str[0] === str.slice(-1) && isPlaindrome(str.slice(1, -1));
+    return str[0] === str.slice(-1) && isPalindrome(str.slice(1, -1));
   }
 }
 
 function isPal(str) {
   // return True if s is palindrome and False otherwise
-  return isPlaindrome(toChars(str));
+  return isPalindrome(toChars(str));
 }
 
-console.log(isPal("Civic"));
-console.log(isPal("Madam"));
-console.log(isPal("Dennis sinned"));
-console.log(isPal("Able was I ere I saw Elba"));
+// console.log(isPal("Civic"));
+// console.log(isPal("Madam"));
+// console.log(isPal("Dennis sinned"));
+// console.log(isPal("Able was I ere I saw Elba"));
